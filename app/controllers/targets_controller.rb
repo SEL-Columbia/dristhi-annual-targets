4bafbffcd722f93728d895569f6d6e491903c837
sudhirv <sudhirv@thoughtworks.com>
2015-07-05 19:13:14 +0530

    Sudhir | Minor clean up.

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -34,9 +34,20 @@ class TargetsController < ApplicationController
         add_error_record(anm_identifier, indicator, target, start_date, end_date)
         next
       end
-
     end
+  end
 
+  def open_spreadsheet(file)
+    case File.extname(file.original_filename)
+      when ".csv" then
+        Roo::Csv.new(file.path, nil, :ignore)
+      when ".xls" then
+        Roo::Excel.new(file.path)
+      when ".xlsx" then
+        Roo::Excelx.new(file.path)
+      else
+        raise "Unknown file type: #{file.original_filename}"
+    end
   end
 
   def sanitise(row)
@@ -66,18 +77,5 @@ class TargetsController < ApplicationController
     error_record[:anm_identifier] = anm_identifier
     @error_records.append(error_record)
     @count = @count + 1
-  end
-
-  def open_spreadsheet(file)
-    case File.extname(file.original_filename)
-      when ".csv" then
-        Roo::Csv.new(file.path, nil, :ignore)
-      when ".xls" then
-        Roo::Excel.new(file.path)
-      when ".xlsx" then
-        Roo::Excelx.new(file.path)
-      else
-        raise "Unknown file type: #{file.original_filename}"
-    end
   end
 end
